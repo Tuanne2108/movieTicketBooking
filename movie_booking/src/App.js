1b4d519c7058cea22f84d32547a8ca48693f3a23
tuan_nguyen <nguyenhoangminhtuan210802@gmail.com>
2024-04-30 22:49:44 +0700

    first-commit

--- movie_booking/src/App.js
+++ movie_booking/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Component/Navbar/navbar';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navbar/>
     </div>
   );
 }
