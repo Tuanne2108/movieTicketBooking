8fc40410c02fdfe2c870abcca40f2b63d03a1967
Tran Thuan <dtthuan.1211@gmail.com>
2024-05-03 16:12:31 +0700

    update
    setting update slider mainpage

---
+++ movie_booking/src/Components/Slider/FilmSlider.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./CardSlider.css";
+import Poster from '../Assets/Poster-NhaBaNu.png'
+
+function FilmSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+  return (
+    <div className="slider-card">
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div className="Poster-Slider">
+            <img src={Poster} alt="" className="Poster-Film"/>
+            <h3>Film moi nhat 1</h3>
+          </div>
+          <div className="Poster-Slider">
+            <img src={Poster} alt="" className="Poster-Film"/>
+            <h3>Film moi nhat 2</h3>
+          </div>
+          <div className="Poster-Slider">
+            <img src={Poster} alt="" className="Poster-Film"/>
+            <h3>Film moi nhat 3</h3>
+          </div>
+          <div className="Poster-Slider">
+            <img src={Poster} alt="" className="Poster-Film"/>
+            <h3>Film moi nhat 4</h3>
+          </div>
+          <div className="Poster-Slider">
+            <img src={Poster} alt="" className="Poster-Film"/>
+            <h3>Film moi nhat 5</h3>
+          </div>
+          <div className="Poster-Slider">
+            <img src={Poster} alt="" className="Poster-Film"/>
+            <h3>Film moi nhat 6</h3>
+          </div>
+          
+        </Slider>
+    </div>
+    </div>
+    
+  );
+}
+
+export default FilmSlider;
--- movie_booking/src/Pages/Cnema.jsx
+++ movie_booking/src/Pages/Cnema.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from '../Components/Navbar/Navbar';
+import SearchingBar from '../Components/Navbar/SearchingBar'
+import FilmSlider from '../Components/Slider/FilmSlider';
+import '../Styles/styles.css'
+import '../Pages/styles/Cnema.css'
+
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Cnema = () => {
   return (
-    <div>
-      
+    <div className="homepage-cnema">
+      <Navbar />
+      <SearchingBar />
+      <FilmSlider />
     </div>
   )
 }
