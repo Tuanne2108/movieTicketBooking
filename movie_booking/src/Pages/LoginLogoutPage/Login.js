0d889e02afab7d74b1e13225701a0abb6ad546eb
Tran Thuan <dtthuan.1211@gmail.com>
2024-05-17 21:49:04 +0700

    update loginpage
    a little bit update login page (header,css)

--- movie_booking/src/Pages/LoginLogoutPage/Login.js
+++ movie_booking/src/Pages/LoginLogoutPage/Login.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './styleLoginLogoutPage.css'
+
 
 const Login = () => {
   return (
-    <div className="cnema-loginpage">Login</div>
-  )
-}
+    <div className="cnema-loginpage">
+      <div className="cnema-loginpage-container">
+        <div className="form-login-container">
+          <div className="login-title">
+            Login
+          </div>
+          <div className="login-form">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Let us know your Email" />
+                <div id="emailHelp" className="form-text custome">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Keep it secret...."/>
+              </div>
+              <div className="button-group">
+              <button type="submit" className="btn btn-primary">Log in</button>
+              
+              <div id="donotHaveAccYet" className="form-text custome">Don't have an account yet?</div>
+              <button type="submit" className="btn btn-primary">Sign up</button>
+              </div>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+      
+      
+    </div>
+  );
+};
 
-export default Login
+export default Login;
