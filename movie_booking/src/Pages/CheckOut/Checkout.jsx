6fbe4673fa617421daa755a0c10921444c309691
Tran Thuan <dtthuan.1211@gmail.com>
2024-06-17 14:56:17 +0700

    final
    checkout

--- movie_booking/src/Pages/CheckOut/Checkout.jsx
+++ movie_booking/src/Pages/CheckOut/Checkout.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './styleCheckout.css'
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div>
+      <div>
+        <div className="Thankyou">
+          <h1>Thank You</h1>
+          <span>Your Booking is successful. Please check your email</span>
+          <p>Enjoy and stay with us</p>
+        </div>
+      </div>
+      </div>
   )
 }
 
